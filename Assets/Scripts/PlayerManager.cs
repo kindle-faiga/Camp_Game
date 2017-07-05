8cc061e7c56d9bf126ec0e3c21233fd440af440a
Toyama Shuma <ts.gold.fire@gmail.com>
2017-07-05 15:55:39 +0900

    動きを滑らかに

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,6 +5,7 @@ namespace CampGame
     [RequireComponent(typeof(RigidbodyManager))]
     public class PlayerManager : MonoBehaviour
     {
+        public static readonly float THRESHOLD = 0.001f;
         private RigidbodyManager rigidbodyManager;
 		private Transform shotPoint;
         private Transform groundPoint;
@@ -36,28 +37,11 @@ namespace CampGame
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
-            if(Mathf.Abs(x) > Mathf.Abs(z))
-            {
-                if (x < -0.1f)
-                {
-                    transform.eulerAngles = new Vector3(0, -90.0f, 0);
-                }
-                else if (0.1f < x)
-                {
-					transform.eulerAngles = new Vector3(0, 90.0f, 0);
-                }
-            }
-            else
-            {
-                if (z < -0.1f)
-				{
-					transform.eulerAngles = new Vector3(0, 180.0f, 0);
-				}
-				else if (0.1f < z)
-				{
-					transform.eulerAngles = new Vector3(0, 0.0f, 0);
-				}
-            }
+            if (THRESHOLD < Mathf.Abs(x) || THRESHOLD < Mathf.Abs(z))
+			{
+				var direction = new Vector3(x, 0, z);
+				transform.localRotation = Quaternion.LookRotation(direction);
+			}
 
             if (isGround)
             {
