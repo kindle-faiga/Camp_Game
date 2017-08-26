5a73fb81e776a036d6c327336822d12096d77737
Toyama Shuma <ts.gold.fire@gmail.com>
2017-08-26 16:59:36 +0900

    no message

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace CampGame
 {
@@ -6,12 +7,15 @@ namespace CampGame
     public class PlayerManager : MonoBehaviour
     {
         public static readonly float THRESHOLD = 0.001f;
+        [SerializeField]
+        private float shotTime = 1.0f;
         private RigidbodyManager rigidbodyManager;
 		private Transform shotPoint;
         private Transform groundPoint;
         private BulletManager bulletManager;
 
         private bool isGround = true;
+        private bool isShot = false;
 
         private void Start()
         {
@@ -29,6 +33,7 @@ namespace CampGame
             if (!bulletManager.GetIsActive() && Input.GetButtonDown("Shot"))
             {
                 StartCoroutine(bulletManager.Shot(shotPoint.position, transform.eulerAngles.y));
+                StartCoroutine(WaitForShot());
             }
         }
 
@@ -37,13 +42,13 @@ namespace CampGame
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
-            if (THRESHOLD < Mathf.Abs(x) || THRESHOLD < Mathf.Abs(z))
+            if (!isShot && (THRESHOLD < Mathf.Abs(x) || THRESHOLD < Mathf.Abs(z)))
 			{
-				var direction = new Vector3(x, 0, z);
-				transform.localRotation = Quaternion.LookRotation(direction);
+                Vector3 direction = new Vector3(x, 0, z);
+                transform.localRotation = Quaternion.LookRotation(direction);
 			}
 
-            if (isGround)
+            if (isGround && !isShot)
             {
                 Vector3 distance = new Vector3(x, 0, z);
 
@@ -54,5 +59,12 @@ namespace CampGame
                 rigidbodyManager.Stop();
             }
         }
+
+		public IEnumerator WaitForShot()
+		{
+			isShot = true;
+			yield return new WaitForSeconds(shotTime);
+			isShot = false;
+		}
     }
 }
